bf48a642e7d159c1dd6f4aeeeafd423687885165
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2025-01-12 15:05:54 +0530

    mm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,9 @@ import { BrowserRouter as Router, Route,Routes,Navigate  } from 'react-router-do
 import Home from './pages/Home'
 import Register from './Auth/Register'
 import Login from './Auth/Login'
-<<<<<<< HEAD
 import Profile from './pages/Profile'
-=======
 import Admin from './pages/Admin'
 import Team from './pages/Team'
->>>>>>> 2b2b91ee44c72cd149b4a6cafc63989b9449dcac
 
 const App = () => {
   return (
@@ -18,12 +15,8 @@ const App = () => {
             <Route path='/' element={<Home></Home>}></Route>
             <Route path='/register' element={<Register></Register>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
-<<<<<<< HEAD
-            <Route path='/profile' element={<Profile></Profile>}></Route>
-=======
             <Route path='/admin/member' element={<Admin></Admin>} />
             <Route path='/member/:userId' element={<Team></Team>} />
->>>>>>> 2b2b91ee44c72cd149b4a6cafc63989b9449dcac
         </Routes>
       </Router>
     </div>
