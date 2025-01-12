5bcc075c011825bf34b182c64441f3f4d99a4451
Honey-Joe <honeyjoe942@gmail.com>
2025-01-12 11:34:49 +0530

    sdd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Router>
         <Routes>
             <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/register ' element={<Register></Register>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
         </Routes>
       </Router>
