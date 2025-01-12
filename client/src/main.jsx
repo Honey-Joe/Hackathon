2b691e0acd97125d33c885b313a22878de08239e
Honey-Joe <honeyjoe942@gmail.com>
2025-01-12 11:27:28 +0530

    final and dont change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,39 +5,7 @@ import React from "react"
 
 
 
-const Applayout = ()=>{
-  return(
-    <>
-      <Header></Header>
-      <Body></Body>
-      <Footer></Footer>
-    </>
-  )
-}
-const  appRouter = createBrowserRouter([
-  {
-    path:"/",
-    element:<Applayout></Applayout>
-  },
-  {
-    path:"/event/:id",
-    element:<EventDetails></EventDetails>
-  },
-  {
-    path:"/nonevent/:id",
-    element:<NonEventDetails></NonEventDetails>
-  },
-  {
-    path:"/register",
-    element:<Register></Register>
-  },{
-    path:"/login",
-    element:<Login></Login>
-  }
-  
- 
-])
-6093
+
 root= ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
