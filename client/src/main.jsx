1c1ae0ce3cf363b13f4c06ed752b4694cfa5aaac
Honey-Joe <honeyjoe942@gmail.com>
2025-01-12 11:31:29 +0530

    sads

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,10 @@ import App from "./App";
 import React from "react"
 
 
-
-
-root= ReactDOM.createRoot(document.getElementById("root"));
+const  root= ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <App></App>
   </React.StrictMode>
-);
+)
