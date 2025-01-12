3c256c9263ebac584dbe6e5fbc4b0d965689e4e5
Honey-Joe <honeyjoe942@gmail.com>
2025-01-12 11:29:02 +0530

    created login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route,Routes,Navigate  } from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './Auth/Register'
+import Login from './Auth/Login'
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Router>
         <Routes>
             <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/register ' element={<Register></Register>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
         </Routes>
       </Router>
     </div>
