0a516e7cada3d8ee29ef88e3b611c1c111cd53fa
Honey <honeyjoe942@gmal.com>
2025-01-30 15:20:23 +0530

    fsd

--- client/src/BASE_URL.js
+++ client/src/BASE_URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://hackathon-chi-lilac.vercel.app"
+export const BASE_URL = "https://hackathon-drab-three.vercel.app"
 // export const BASE_URL = "http://localhost:3000"
