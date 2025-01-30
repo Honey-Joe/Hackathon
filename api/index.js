026ba296f704addb1da5b853d9c3001035db280f
Honey <honeyjoe942@gmal.com>
2025-01-30 14:45:29 +0530

    sdf

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   }));
 
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change '*' to a specific domain if needed
+    res.setHeader('Access-Control-Allow-Origin', 'https://hackathon-25.vercel.app/'); // Change '*' to a specific domain if needed
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
